25236a62f9e9cd387898fa4e50b27b6f831c1c54
Alan Huynh <alan.hanhuynh@gmail.com>
2020-11-17 11:53:56 -0700

    ajaxCall

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const searchButton = $('#search-btn');
-
+let cityName = $('#search').val().trim();
 searchButton.on('click', function(e){
     e.preventDefault();
     console.log('hi guys');
-
+    buildQueryURL();
 });
+
+function buildQueryURL() {
+    var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
+    // $.ajax({
+        // url: queryURL,
+        // method: "GET"
+    // })
+    console.log(queryURL);
+}
