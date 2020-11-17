28ae36f3337bacca6b9456230cbcae6ad0fda104
Alan Huynh <alan.hanhuynh@gmail.com>
2020-11-17 14:47:00 -0700

    added more style

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ $(document).ready(function () {
     var cityName = $("#search").val().trim();
     console.log(cityName);
     buildQueryURL();
+    $(".card-content").empty();
 
     function buildQueryURL() {
       var queryURL =
         "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
-     
+
       $.ajax({
         url: queryURL,
         method: "GET",
       }).then(function (data) {
-        
+
         for (let i = 0; i < data.length; i++) {
           let breweryName = $("<p>")
             .attr("class", "breweryData")
@@ -24,11 +25,11 @@ $(document).ready(function () {
             .attr("class", "breweryData")
             .text("Brewery Address: " + data[i].street);
 
-            let breweryPhone = $("<p>")
+          let breweryPhone = $("<p>")
             .attr("class", "breweryData")
             .text("Brewery Phone: " + data[i].phone);
 
-            let breweryWebsite = $("<p>")
+          let breweryWebsite = $("<p>")
             .attr("class", "breweryData")
             .text("Brewery Website: " + data[i].website_url);
 
@@ -37,5 +38,22 @@ $(document).ready(function () {
         }
       });
     }
+
+    const settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/search?query=null",
+      "method": "GET",
+      "headers": {
+        "accept": "application/json",
+        "x-rapidapi-key": "0649af007dmsh11902b4404b18f5p109e05jsne2a7ef3b2d3f",
+        "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
+      }
+    };
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      let joke = $("<p>").text(response.result[0].value);
+       $(".card-content").append(joke);
+    });
   });
 });
