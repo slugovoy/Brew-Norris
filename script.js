f26c92d02787a1971f25c81ba1e20f90e182d383
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-11-17 13:25:00 -0700

    added functions

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const searchButton = $('#search-btn');
-let cityName = $('#search').val().trim();
-searchButton.on('click', function(e){
+$(document).ready(function () {
+  const searchButton = $("#search-btn");
+  searchButton.on("click", function (e) {
     e.preventDefault();
-    console.log('hi guys');
+    var cityName = $("#search").val().trim();
+    console.log(cityName);
     buildQueryURL();
-});
 
-function buildQueryURL() {
-    var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
-    // $.ajax({
-        // url: queryURL,
-        // method: "GET"
-    // })
-    console.log(queryURL);
-}
+    function buildQueryURL() {
+      var queryURL =
+        "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
+     
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (data) {
+        
+        for (let i = 0; i < data.length; i++) {
+          let breweryName = $("<p>")
+            .attr("class", "breweryData")
+            .text("Brewery: " + data[i].name);
+          console.log(data[i].name);
+          
+          $(".results-container").append(breweryName);
+        }
+      });
+    }
+  });
+});
